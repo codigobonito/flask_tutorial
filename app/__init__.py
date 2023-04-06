5abcf39c4b24110b75eb926610c75f37c7fb03ac
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-04-06 11:47:22 -0300

    Add chapter 5

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+login = LoginManager(app)
+login.login_view = "login"  # name of the login  view function
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db)  # Migrate is a wrapper around Alembic
 
 
 from app import routes, models
