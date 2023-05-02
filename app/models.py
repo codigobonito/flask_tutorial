93472b9a0c802192dcc654851b2a1993494c9d9d
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-05-02 13:51:10 -0300

    Add code for chapter 6

--- app/models.py
+++ app/models.py
@@ -6,13 +6,14 @@ from flask_login import UserMixin
 from hashlib import md5
 
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -25,6 +26,7 @@ class User(UserMixin, db.Model):
         return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
             digest, size
         )
+
     def __repr__(self):
         return "<User {}>".format(self.username)
 
