9ff47d6fae632d5b0c1ee13ec8208e99e8511fff
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-05-11 18:00:53 -0300

    Add Chapter 9 in full

--- app/routes.py
+++ app/routes.py
@@ -92,7 +92,16 @@ def index():
     posts = current_user.followed_posts().paginate(
         page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
     )
-    return render_template("index.html", title="Home", form=form, posts=posts.items)
+    next_url = url_for("index", page=posts.next_num) if posts.has_prev else None
+    prev_url = url_for("index", page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        "index.html",
+        title="Home",
+        form=form,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.route("/explore")
@@ -102,7 +111,15 @@ def explore():
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
     )
-    return render_template("index.html", title="Explore", posts=posts.items)
+    next_url = url_for("explore", page=posts.next_num) if posts.has_next else None
+    prev_url = url_for("explore", page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        "index.html",
+        title="Explore",
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+    )
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -142,13 +159,29 @@ def login():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {"author": user, "body": "Test post #1"},
-        {"author": user, "body": "Test post #2"},
-    ]
-
+    page = request.args.get("page", 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config["POSTS_PER_PAGE"], error_out=False
+    )
+    next_url = (
+        url_for("user", username=user.username, page=posts.next_num)
+        if posts.has_next
+        else None
+    )
+    prev_url = (
+        url_for("user", username=user.username, page=posts.prev_num)
+        if posts.has_prev
+        else None
+    )
     form = EmptyForm()
-    return render_template("user.html", user=user, posts=posts, form=form)
+    return render_template(
+        "user.html",
+        user=user,
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url,
+        form=form,
+    )
 
 
 @app.route("/logout")
